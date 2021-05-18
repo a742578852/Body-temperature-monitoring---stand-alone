f37ef59e1b1a8b05a00be5c2212d989a309ba937
tzt <742578852@qq.com>
2021-05-18 14:58:31 +0800

    first commit

---
+++ components/cg-swiper/cg-swiper.vue
@@ -0,0 +1,84 @@
+<template>
+	<view class="" style="position: relative;">
+		<swiper
+			class="swiper"
+			@change="swiperChange"
+		>
+			<swiper-item style="width: 100%;height: 360upx;">
+				<view class="swiper-item" >
+					<view class="swiper-item-view" v-for="(item, index) in swiperList" :key="index" v-if="index < 20" @click="clickItem(item)">
+						<image class="swiper-img" :src="item.img" ></image>
+						<text>{{ item.text }}</text>
+					</view>
+				</view>
+			</swiper-item>
+			<!-- <block v-if="swiperList.length > 20">
+				<swiper-item style="width: 100%;height: 360upx;">
+					<view class="swiper-item">
+						<view class="swiper-item-view" v-for="(item, index) in swiperList" :key="index" v-if="index > 19 && index < 30" @click="clickItem(item.item)">
+							<image class="swiper-img" :src="item.img"></image>
+							<text>{{ item.text }}</text>
+						</view>
+					</view>
+				</swiper-item>
+			</block> -->
+		</swiper>
+		</view>
+	</view>
+</template>
+
+<script>
+export default {
+	name: 'cg-swiper',
+	props: {
+		// 滑动块视图容器
+		swiperList: {
+			type: Array,
+			default: function() {
+				return [];
+			}
+		},
+		
+	},
+	data() {
+		return {
+			swiperCurrent: 0
+		};
+	},
+	methods: {
+		swiperChange(e) {
+			this.swiperCurrent = e.detail.current;
+		},
+
+		clickItem(item) {
+			this.$emit('clickItem', item);
+		}
+	}
+};
+</script>
+
+<style>
+.swiper {
+	margin-top: 10px;
+	width: 100%;
+	height: 700upx;
+	padding-top: 20upx;
+}
+.swiper-item {
+	display: flex;
+	flex-wrap: wrap;
+}
+.swiper-item-view {
+	display: flex;
+	flex-direction: column;
+	justify-content: center;
+	align-items: center;
+	width: 25%;
+	height: 170upx;
+}
+.swiper-img {
+	width: 100upx;
+	height: 100upx;
+}
+
+</style>
---
+++ pages/index/index.vue
@@ -0,0 +1,113 @@
+<template>
+	<view >
+		<cg-swiper :swiperList="swiperList" @clickItem="clickItem"></cg-swiper>
+		<view style="width: 50%;margin-left: 25%;margin-bottom: 20upx;">
+			<image src="../../static/logo.png" style="height: 400upx;border-radius: 200upx;"></image>
+		</view>
+	</view>
+</template>
+
+<script>
+	import cgSwiper from '@/components/cg-swiper/cg-swiper.vue';
+	export default {
+		components: {
+			cgSwiper,
+		},
+		data() {
+			return {
+				swiperList: [
+					{
+						index:0,
+						text: '卧',
+						img: '../../static/logo.png'
+					},
+					{
+						index:1,
+						text: '坐',
+						img: '../../static/logo.png'
+					},
+					{
+						index:2,
+						text: '走',
+						img: '../../static/logo.png'
+					},
+					{
+						index:3,
+						text: '跑',
+						img: '../../static/logo.png'
+					},
+					{
+						index:4,
+						text: '上肢运动',
+						img: '../../static/logo.png'
+					},
+					{
+						index:5,
+						text: '上下肢运动',
+						img: '../../static/logo.png'
+					},
+					{
+						index:6,
+						text: '吃',
+						img: '../../static/logo.png'
+					},
+					{
+						index:7,
+						text: '写',
+						img: '../../static/logo.png'
+					},
+					{
+						index:8,
+						text: '跑',
+						img: '../../static/logo.png'
+					},
+					{
+						index:9,
+						text: '早上醒来',
+						img: '../../static/logo.png'
+					},
+					{
+						index:10,
+						text: '忧愁',
+						img: '../../static/logo.png'
+					},
+					{
+						index:11,
+						text: '口苦',
+						img: '../../static/logo.png'
+					},
+					{
+						index:12,
+						text: '喜悦',
+						img: '../../static/logo.png'
+					},
+					{
+						index:13,
+						text: '疲劳',
+						img: '../../static/logo.png'
+					},
+					{
+						index:14,
+						text: '听,看',
+						img: '../../static/logo.png'
+					},
+					{
+						index:15,
+						text: '便后',
+						img: '../../static/logo.png'
+					}
+					
+				]
+			}
+		},
+		methods: {
+			clickItem(item) {
+				console.log(item.index);
+			}
+		}
+	}
+</script>
+
+<style>
+
+</style>
