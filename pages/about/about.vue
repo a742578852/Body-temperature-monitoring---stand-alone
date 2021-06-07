99e2c15005d657f66142493d7af9384458210693
“15852288019” <15852288019@163.com>
2021-06-07 13:23:59 +0800

    导航栏和关于我们

--- pages/about/about.vue
+++ pages/about/about.vue
@@ -1,25 +1,25 @@
 <template>
 	<view>
 		<view class="" style="display: flex;align-items: center;justify-content: center;flex-direction: column;font-size: 50upx;">
-			<image src="../../static/home-selected.png" style="width: 150upx;height: 150upx;"></image>
-			<text>智能体温计</text>
+			<image src="../../static/logo.png" style="width: 150upx;height: 150upx;"></image>
+			<text>对称平衡</text>
 		</view>
 		<view class="" style="margin-top: 150upx;">
 		<view class="cu-form-group" >
 			<view class="title">版本:</view>
-			<input  name="input" >1.0.1</input>
+			<input  name="input" disabled>1.0.0</input>
 		</view>
-		<view class="cu-form-group">
+		<!-- <view class="cu-form-group">
 			<view class="title">版权:</view>
-			<input  name="input" >所有者</input>
-		</view>
+			<input  name="input" disabled>所有者</input>
+		</view> -->
 		<view class="cu-form-group" @click="serve">
 			<view class="title">服务协议:</view>
-			<u-icon name="arrow-right"></u-icon>
+			<u-icon name="arrow-right" disabled></u-icon>
 		</view>
 		<view class="cu-form-group" @click="yinsi">
 			<view class="title">隐私协议:</view>
-			<u-icon name="arrow-right"></u-icon>
+			<u-icon name="arrow-right" disabled></u-icon>
 		</view>
 		</view>
 	</view>
